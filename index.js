7a96c99dd19d22bc60526635b4074fc17750d1f2
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2024-02-19 19:09:12 +0600

    end of append chile

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ document.getElementById('btn-ticket').addEventListener('click',function(){
     nextsection.scrollIntoView({behavior: 'smooth'})
 })
 const allSeat = document.getElementsByClassName("seat-no");
-
-const classType = 'economy';
-        const price = 550;
 let count = 0;
 let selectSeat = 8
 for( const btn of allSeat){
     btn.addEventListener("click",function (e) {
-        
       // console.log(e.target.parentNode.childNodes[1].innerText)
         if(count <4 ){count = count + 1;
             setInnertext('Selected-seat',count);
@@ -19,6 +15,20 @@ for( const btn of allSeat){
             selectSeat = selectSeat - 1;
        setInnertext('seat-left',selectSeat)
         }
+        //
+        const price = e.target.parentNode.childNodes[1].innerText;
+        console.log(price)
+        const selectedCont = document.getElementById('select-seat');
+        const li = document.createElement('li');
+        const p =document.createElement('p');
+        p.innerText = price;
+        li.appendChild(p);
+        selectedCont.appendChild(li);
+
+
+
+        //Total section
+       
     })
 
 }
@@ -37,3 +47,29 @@ buttons.forEach(button => {
 function setInnertext (id , value){
     document.getElementById(id).innerText = value;
 };
+//butt disable;
+function handleClick1(){
+    document.getElementById("button-1").disabled = true;
+}
+function handleClick2(){
+    document.getElementById("button-2").disabled = true;
+}
+function handleClick3(){
+    document.getElementById("button-3").disabled = true;
+}
+function handleClick4(){
+    document.getElementById("button-4").disabled = true;
+}
+function handleClick5(){
+    document.getElementById("button-5").disabled = true;
+}
+function handleClick6(){
+    document.getElementById("button-6").disabled = true;
+}
+function handleClick7(){
+    document.getElementById("button-7").disabled = true;
+}
+function handleClick8(){
+    document.getElementById("button-8").disabled = true;
+}
+//tOTAL PRICE FUNCTION
